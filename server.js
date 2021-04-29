f934e7dbe7c40118dd130cd2a77db8d578c0c774
Eric Treadway <eric.s.treadway@gmail.com>
2021-04-29 10:43:35 -0500

    Added Create Endpoint

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const pgp = require("pg-promise")();
 const db = pgp("postgres://qomyoved:ZhyvOedvW_8knyyEzPqIhqORWtuxZ_ER@drona.db.elephantsql.com:5432/qomyoved");
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded());
 
+//CRUD Endpoints
+//create
+app.post('/foods', async (req, res) => {
+  const ndb_no = req.body.ndb_no;
+  const fdgrp_cd = req.body.fdgrp_cd;
+  const long_desc = req.body.long_desc;
+  const shrt_desc = req.body.shrt_desc;
+  const survey = req.body.survey;
+  const refuse = req.body.refuse;
+  const n_factor = req.body.n_factor;
+  const pro_factor = req.body.pro_factor;
+  const fat_factor = req.body.fat_factor;
+  const cho_factor = req.body.cho_factor;
+  const comname = req.body.comname;
+  const manufacname = req.body.manufacname;
+  const ref_desc = req.body.ref_desc;
+  const sciname = req.body.sciname;
 
+  await db.none("insert into food_des (ndb_no, fdgrp_cd, long_desc, shrt_desc, survey, refuse, n_factor, pro_factor, fat_factor, cho_factor, comname, manufacname, ref_desc, sciname) Values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
+    [
+      ndb_no, fdgrp_cd, long_desc, shrt_desc, survey, refuse, n_factor, pro_factor, fat_factor, cho_factor, comname, manufacname, ref_desc, sciname
+    ]);
+  res.send('Food Description Created!');
+})
 
 app.listen(PORT, () => {
   console.log(`LikeyPix API is running on port ${PORT}`);
