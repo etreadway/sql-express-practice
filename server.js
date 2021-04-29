9334f5e6e21e2fb2115bd19189f73556e4aaacb2
Eric Treadway <eric.s.treadway@gmail.com>
2021-04-29 07:40:54 -0500

    get all teachers

--- server.js
+++ server.js
@@ -7,7 +7,38 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded());
 
+//get all classes
+app.get('/classes', async (req, res) => {
+  const classes = await db.any("SELECT * FROM classes").then((classes) => {
+    return classes;
+  });
+  res.send(classes);
+});
 
+//get all students
+app.get('/students', async (req, res) => {
+  const students = await db.any('SELECT * FROM students').then((students) => {
+    return students;
+  });
+  res.send(students);
+});
+
+//get student by class id
+app.get('/students/:id', async (req, res) => {
+  const id = req.params.id;
+  const students = await db.any('SELECT * FROM students WHERE class_id = $1', [id]).then((students) => {
+    return students;
+  }); 
+  res.send(students);
+});
+
+//get all teachers
+app.get('/teachers', async (req, res) => {
+  const teachers = await db.any('SELECT * FROM teachers').then((teachers) => {
+    return teachers;
+  });
+  res.send(teachers);
+});
 
 app.listen(PORT, () => {
   console.log(`LikeyPix API is running on port ${PORT}`);
