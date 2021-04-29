c300d3adf384f7c58510b61a0a7b425420fd34d8
etreadway <68955513+etreadway@users.noreply.github.com>
2021-04-28 20:34:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const pgp = require("pg-promise")();
 const db = pgp("postgres://azhfhwpn:5VPylMsT0XOBlEO8RxxKXAOjumxSDY6u@queenie.db.elephantsql.com:5432/azhfhwpn");
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded());
 
 
 
